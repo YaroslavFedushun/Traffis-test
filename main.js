4b691aeb677c50d481d909d51ceb33a8efd74428
YaroslavFedushun <salakar88@gmail.com>
2023-08-23 23:44:36 +0300

    graduated from logic

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const Swal = require('sweetalert2')
+
+
+
 const urlParams = new URLSearchParams(window.location.search);
 const app_offer_link = urlParams.get('app_offer_link') || ' https://www.google.com/';
 
@@ -7,8 +11,25 @@ function goToPlayMarket() {
     window.open(app_offer_link, '_blank').focus();
 }
 
-function smoothScroll(id){
+function smoothScroll(id) {
     document.querySelector(id).scrollIntoView({
         behavior: 'smooth'
     });
+}
+
+function send() {
+    const form = document.querySelector('#form');
+    const formData = new FormData(form);
+    const result = {};
+    for (let [name, value] of formData) {
+        result[name] = value;
+    }
+    if (result.name == "" || result.email== "" || result.message == ""  ) {
+
+        swal("Oops!", "Fill the blanks correctly!", "error" );
+    }
+    else
+    swal("Great!", "We'll get back to you soon!", "success");
+    console.log(formData);
+    console.log(result);
 }
